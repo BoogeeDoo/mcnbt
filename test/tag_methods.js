08945fa145a3023e115c29166a8995473f4d9fc4
XadillaX <admin@xcoder.in>
2014-12-30 16:58:21 +0800

    tag methods getter

---
+++ test/tag_methods.js
@@ -0,0 +1,30 @@
+/**
+ * XadillaX created at 2014-12-30 16:17:32
+ *
+ * Copyright (c) 2014 XadillaX' Gensokyo, all rights
+ * reserved
+ */
+var nbt = require("../tags/tags");
+var BaseTag = require("../tags/base");
+
+// Base
+console.log("BaseTag:");
+for(var key in BaseTag.prototype) {
+    if(typeof(BaseTag.prototype[key]) === "function") {
+        console.log("  " + (key[0] === "_" ? "- " : "+ ") + key);
+    }
+}
+
+// Each other's
+for(var i = 1; i <= 11; i++) {
+    var tag = new nbt[i]();
+    console.log(tag.getType() + ":");
+
+    for(var key in tag) {
+        if(undefined === BaseTag.prototype[key] &&
+           typeof(tag[key]) === "function") {
+            console.log("  " + (key[0] === "_" ? "- " : "+ ") + key);
+        }
+    }
+}
+
